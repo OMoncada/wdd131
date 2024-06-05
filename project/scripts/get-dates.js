8d58804d2a4c92f3654be85ec9c34bad81ca06d0
OMoncada <132164886+OMoncada@users.noreply.github.com>
2024-06-05 11:57:30 -0400

    Update get-dates.js
    guardar cambios porfavor

--- project/scripts/get-dates.js
+++ project/scripts/get-dates.js
@@ -1,14 +1,22 @@
-// References on how to generate the year:
-// https://codepen.io/blazzard-jason/pen/LYrqxGJ
-const copyrightYear = {
-    year: "numeric"
-};
+document.addEventListener("DOMContentLoaded", function () {
+    // References on how to generate the year:
+    // https://codepen.io/blazzard-jason/pen/LYrqxGJ
+    const copyrightYear = {
+        year: "numeric"
+    };
 
-document.querySelector('footer #year').textContent = new Date().toLocaleDateString("en-US", copyrightYear);
+    const yearElement = document.querySelector('footer #year');
+    if (yearElement) {
+        yearElement.textContent = new Date().toLocaleDateString("en-US", copyrightYear);
+    }
 
-// References to show the date and time the document was last modified
-// https://www.tutorialspoint.com/How-to-show-the-date-and-time-the-document-was-last-modified-with-JavaScript
-// https://developer.mozilla.org/en-US/docs/Web/API/Document/lastModified
-let lastModificationDate = document.lastModified;
+    // References to show the date and time the document was last modified
+    // https://www.tutorialspoint.com/How-to-show-the-date-and-time-the-document-was-last-modified-with-JavaScript
+    // https://developer.mozilla.org/en-US/docs/Web/API/Document/lastModified
+    let lastModificationDate = document.lastModified;
+    const lastModifiedElement = document.querySelector('#lastModified');
 
-document.querySelector('#lastModified').innerHTML = `Last Modification: ${lastModificationDate}`;
+    if (lastModifiedElement) {
+        lastModifiedElement.innerHTML = `Last Modification: ${lastModificationDate}`;
+    }
+});
