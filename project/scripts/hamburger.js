8ab1e9ca090fd64f094c595c277d0da91c69be13
OMoncada <132164886+OMoncada@users.noreply.github.com>
2024-06-05 15:12:07 -0400

    Delete project directory
    eliminar necesito hacerlo

--- project/scripts/hamburger.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const hamburger = document.getElementById('hamburger');
-    const menu = document.querySelector('.hamburger-menu');
-
-    hamburger.addEventListener('click', function () {
-        menu.classList.toggle('active');
-    });
-});
